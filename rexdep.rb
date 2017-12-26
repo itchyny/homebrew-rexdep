ccfbbff2052b9cb652a8532d4d36f71eff9386b0
itchyny <itchyny@hatena.ne.jp>
2017-12-26 22:50:39 +0900

    rexdep v0.0.9

--- rexdep.rb
+++ rexdep.rb
@@ -1,13 +1,13 @@
 class Rexdep < Formula
   homepage 'https://github.com/itchyny/rexdep'
-  version '0.0.8'
+  version '0.0.9'
 
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/itchyny/rexdep/releases/download/v0.0.8/rexdep_darwin_amd64.zip'
-    sha256 'd7b9c81a943fd1a20c0b1bcfc55e15bf479aa13fdd99aa489503874ff16b8d8a'
+    url 'https://github.com/itchyny/rexdep/releases/download/v0.0.9/rexdep_darwin_amd64.zip'
+    sha256 '2ffa3003010a45e97dc0fe2cb0bbc38b6121ff43adb1c590bc072cdffd9a14a9'
   else
-    url 'https://github.com/itchyny/rexdep/releases/download/v0.0.8/rexdep_darwin_386.zip'
-    sha256 '8f202ecaa70364a2ce027d26c6f6c1bb5e00a619efa55f06ae16a7837307d7b9'
+    url 'https://github.com/itchyny/rexdep/releases/download/v0.0.9/rexdep_darwin_386.zip'
+    sha256 '35b762ab595924ad9f3b97df4e51d4aa046ce1f9e030311bdc376659feb6c1b9'
   end
 
   head do
